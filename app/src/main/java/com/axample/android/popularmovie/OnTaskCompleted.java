79f0eee67debbb110cc957c83f179025727eea70
imamaris <imam.aris94@gmail.com>
2017-07-10 16:48:13 +0700

    Initial Commit

---
+++ app/src/main/java/com/axample/android/popularmovie/OnTaskCompleted.java
@@ -0,0 +1,9 @@
+package com.axample.android.popularmovie;
+
+/**
+ * Created by imam.munandar on 07/10/17.
+ */
+
+interface OnTaskCompleted {
+    void onFetchMoviesTaskCompleted(Movie[] movies);
+}
